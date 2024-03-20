bdec993451eaa1d16386cc8dde7867d89d7ec1ea
Abdul Raheem <156960089+AbdulRaheem15@users.noreply.github.com>
2024-03-20 20:32:14 +0000

    variables in javascript.

--- 01-basics/test.js
+++ 01-basics/test.js
@@ -1,1 +1,29 @@
-console.log("A.Raheem");
+var mat1=[
+    [4,8],
+    [3,7]
+];
+var mat2=[
+    [2,4],
+    [1,5]
+];
+
+document.write("Given Matrix are:")
+document.write("*** Matrix 1***")
+
+var i,j;
+for(i=0;i<2;i++){
+    for(j=0;j<2;j++){
+        document.write(mat1[i][j]+"&nbsp &nbsp &nbsp &nbsp")
+    }
+    document.write("<br>");
+}
+document.write("*** Matrix 2***")
+
+var i,j;
+for(i=0;i<2;i++){
+    for(j=0;j<2;j++){
+        document.write(mat2[i][j]+"&nbsp &nbsp &nbsp &nbsp")
+    }
+    document.write("<br>");
+}
+
