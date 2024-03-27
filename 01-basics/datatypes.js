de60e551cf4949feb7d0c009e0b85b890132557e
Abdul Raheem <156960089+AbdulRaheem15@users.noreply.github.com>
2024-03-27 20:55:16 +0000

    Data types.

---
+++ 01-basics/datatypes.js
@@ -0,0 +1,12 @@
+let name = "Abdul Raheem"; 
+let age = "20";
+
+// number (use for showing the number in the data)
+// bigint (use for showing the large number in the data)
+// string (The text which is in the commas.)
+// null (when nothing is present is in parathesis)
+// undefined (when the value is not assign).
+// symbol (uniqueness)
+// boolean (true/false)
+
+console.log(typeof(age));
